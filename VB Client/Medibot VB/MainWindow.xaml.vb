cf91830fe2a0767dfd5160f6eb6a3212f31b57c4
Jake Davis <jakeyjdavis@gmail.com>
2017-02-27 16:46:53 +0000

    Added VB Client

---
+++ VB Client/Medibot VB/MainWindow.xaml.vb
@@ -0,0 +1,6 @@
+﻿Class MainWindow
+    Sub Init() Handles Me.Initialized
+        lbMedibots.Items.Add("Medibot 1")
+        wbCameraStream.Source = New Uri("https://google.co.uk")
+    End Sub
+End Class
